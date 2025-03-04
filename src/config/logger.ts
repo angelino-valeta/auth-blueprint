2c104646e5b50dd6b66047df43e19e99695f6e91
Angelino Valeta <angelinovaleta@gmail.com>
2025-03-04 22:18:58 +0100

    chore: Update config

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,16 +1,15 @@
 import * as winston from 'winston';
-import Transport from 'winston-transport'; // Importar a classe base para transports
+import Transport from 'winston-transport';
 import { getAppDataSource } from './database';
 import { Log } from '../infrastructure/entities/Log';
 
-// Transport personalizado para salvar logs no banco
 class DatabaseTransport extends Transport {
   constructor() {
     super();
   }
 
   async log(info: any, callback: () => void) {
-    setImmediate(() => this.emit('logged', info)); // Emite o evento 'logged' assincronamente
+    setImmediate(() => this.emit('logged', info));
     const database = await getAppDataSource()
     try {
       if (!database.isInitialized) {
@@ -18,24 +17,23 @@ class DatabaseTransport extends Transport {
       }
       const logRepo = database.getRepository(Log);
       await logRepo.save({
-        level: info.level,
-        message: info.message,
-        timestamp: info.timestamp,
-        trace_id: info.traceId || 'N/A',
+        level: info?.level,
+        message: info?.message,
+        timestamp: info?.timestamp,
+        trace_id: info?.traceId || 'N/A',
       });
     } catch (err) {
-      console.error('Erro ao salvar log no banco:', err); // Fallback para não quebrar
+      console.error('Erro ao salvar log no banco:', err);
     }
 
-    callback(); // Chama o callback para indicar que o log foi processado
+    callback();
   }
 }
 
-// Criação do logger
 const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || (process.env.NODE_ENV === 'production' ? 'info' : 'debug'),
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
     winston.format.json()
   ),
   transports: [
@@ -48,15 +46,19 @@ const logger = winston.createLogger({
     new winston.transports.File({
       filename: process.env.ERROR_LOG_FILE || 'logs/error.log',
       level: 'error',
-      maxsize: 5_242_880, // 5MB
-      maxFiles: 5,
+      maxsize: 10485760, // 5MB
+      maxFiles: 10,
+      tailable: true,
+      format: winston.format.timestamp({ format: 'YYYY-MM-DD' }),
+      zippedArchive: true,
     }),
     new winston.transports.File({
       filename: process.env.COMBINED_LOG_FILE || 'logs/combined.log',
-      maxsize: 5_242_880,
-      maxFiles: 5,
+      tailable: true,
+      format: winston.format.timestamp({ format: 'YYYY-MM-DD' }),
+      zippedArchive: true,
     }),
-    new DatabaseTransport(), // Usar o transport personalizado
+    new DatabaseTransport()
   ],
 });
 
